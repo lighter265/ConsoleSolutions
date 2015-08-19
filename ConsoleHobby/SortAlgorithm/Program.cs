2fc880ebe12475e60fef556563970722cd61f277
Satoshi Shimizu <info@blueskyjp.com>
2015-08-19 21:28:40 +0900

    色々

---
+++ ConsoleHobby/SortAlgorithm/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SortAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            int[] list = new int[size];
+            Random r = new Random(DateTime.Now.Millisecond);
+            for(int i = 0; i < size; i++)
+            {
+                list[i] = (r.Next() % size);
+            }
+
+            foreach(var item in list)
+            {
+                Debug.Write(item + " ");
+            }
+            Debug.WriteLine("");
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var asced = Sort.Selection(list, Sort.OrderType.Asc);
+            Debug.WriteLine("Asced.");
+            foreach (var item in asced)
+            {
+                Debug.Write(item + " ");
+            }
+            Debug.WriteLine("");
+
+            Debug.WriteLine(sw.Elapsed);
+
+            var desced = Sort.Selection(list, Sort.OrderType.Desc);
+            Debug.WriteLine("Desced.");
+            foreach (var item in desced)
+            {
+                Debug.Write(item + " ");
+            }
+            Debug.WriteLine("");
+
+            Debug.WriteLine(sw.Elapsed);
+
+            sw.Stop();
+        }
+    }
+
+    public class Sort
+    {
+        public  enum OrderType { Asc, Desc };
+
+        public static Type[] Bubble<Type>(Type[] list, OrderType type)
+            where Type : IComparable
+        {
+            //int sposi = 0;
+            bool contflag = true;
+
+            while(contflag)
+            {
+                contflag = false;
+                for (int i = 0; i < list.Length - 1; i++)
+                {
+                    //i番目とi+1番目を比較し、入れ替えの必要があれば入れ替える
+                    if(Compare(list[i], list[i+1], type))
+                    {
+                        Swap(ref list[i], ref list[i + 1]);
+                        contflag = true;
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        public static Type[] Selection<Type>(Type[] list, OrderType type)
+            where Type : IComparable
+        {
+            for(int i = 0; i < list.Length; i++)
+            {
+                Type temp = list[i];
+                int tposi = i;
+                for(int j = i+1; j < list.Length; j++)
+                {
+                    if (!Compare(temp, list[j], type)) tposi = j;
+                }
+                if (!temp.Equals(list[i])) Swap(ref list[i], ref list[tposi]);
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// 2つの比較可能オブジェクトを比較する。
+        /// </summary>
+        /// <typeparam name="Type"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="t"></param>
+        /// <returns>OrderType.Asc->a Desc->b</returns>
+        public static Type CompareByOrder<Type>(Type a, Type b, OrderType t)
+            where Type : IComparable
+        {
+            if (a.Equals(b)) return a;
+            else
+            {
+                if (t == OrderType.Asc) return (a.CompareTo(b) > 0 ? a : b);
+                else return (a.CompareTo(b) < 0 ? a : b);
+            }
+        }
+
+
+        /// <summary>
+        /// 2つの比較可能オブジェクトを比較する。
+        /// </summary>
+        /// <typeparam name="Type"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>(a=b):0 (a>b):1 (a<b):-1</returns>
+        public static bool Compare<Type> (Type a, Type b, OrderType t)
+            where Type : IComparable
+        {
+            if (a.Equals(b)) return false;
+            else
+            {
+                if (t == OrderType.Asc) return (a.CompareTo(b) > 0 ? true : false);
+                                   else return (a.CompareTo(b) < 0 ? true : false);
+            }
+        }
+
+        /// <summary>
+        /// 2つの値を交換します
+        /// </summary>
+        /// <typeparam name="Type"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static void Swap<Type>(ref Type a, ref Type b)
+        {
+            var tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+
+}
---
+++ ConsoleHobby/makeMaze/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+/// <summary>
+/// 未完成。する予定もなし。
+/// </summary>
+
+namespace makeMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new MakeMaze();
+
+            map.setStart(2, 2);
+            map.Create(15, 19);
+            map.Fill(MakeMaze.WallTypeWall);
+
+            map.Make();
+
+            map.Dump();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Vector2D
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Vector2D(int X, int Y)
+        {
+            this.x = X;
+            this.y = Y;
+        }
+    }
+
+    public class MakeMaze
+    {
+        //定数------------------------------------------
+        private const int _OutOfRange = -1;
+        public const int WallTypeNone = 0;
+        public const int WallTypeWall = 1;
+
+        public const int numTypeEven = 0;
+        public const int numTypeOdd = 1;
+
+
+        //メンバー---------------------------------------
+        private int _width;
+        private int _height;
+        private int[] _map = null;
+
+        private int startx;
+        private int starty;
+
+
+        //アクセサー---------------------------------------
+        public int width
+        {
+            get;
+            protected set;
+        }
+
+        public int height
+        {
+            get;
+            protected set;
+        }
+        //コンストラクタ------------------------------------
+        public MakeMaze()
+        {
+            //nop
+        }
+
+        //公開メソッド---------------------------------------
+        public void setStart(int x, int y)
+        {
+            startx = x;
+            starty = y;
+        }
+
+        public void Create(int w, int h)
+        {
+            width = w;
+            height = h;
+            _map = new int[width * height];
+        }
+        
+        public void Dump()
+        {
+            Console.WriteLine("[Maze Size] (w,h)=({0},{1})", width, height);
+
+            for (int y = 0; y < height; y++)
+            {
+                string str = "";
+                for (int x = 0; x < width; x++)
+                {
+                    str += (getValue(x, y) == WallTypeNone ? "□" : "■");
+                }
+                Console.WriteLine(str);
+            }
+        }
+
+        public void Fill(int v)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for(int x = 0; x < width; x++)
+                {
+                    Set(x, y, v);
+                }
+            }
+        }
+
+        public void Make()
+        {
+            if(!isNumberType(startx, starty, numTypeEven))
+            {
+                Console.WriteLine("Error, setStartでは偶数を指定して下さい。");
+                return;
+            }
+            if (!isNumberType(width, height, numTypeOdd))
+            {
+                Console.WriteLine("Error, Createでは奇数を指定して下さい。");
+                return;
+            }
+
+            Dig(startx, starty);
+        }
+        //非公開メソッド---------------------------------------
+
+        protected int toIndex(int x, int y)
+        {
+            return (y * width) + x;
+        }
+
+        protected bool isOutOfRange(int x, int y)
+        {
+            if (x < 0 || x >= width) return true;
+            if (y < 0 || y >= height) return true;
+            return false;
+        }
+
+        protected int getValue(int x, int y)
+        {
+            if (isOutOfRange(x, y)) return _OutOfRange;
+            return _map[y * width + x];
+        }
+
+        protected void Set(int x, int y, int v)
+        {
+            if (isOutOfRange(x, y)) return;
+            _map[y * width + x] = v;
+        }
+
+        protected bool isNumberType(int x, int y, int t)
+        {
+            if ((x % 2 == t) && (y % 2 == t)) return true;
+            return false;
+        }
+
+        protected void Dig(int x, int y)
+        {
+            Set(x, y, WallTypeNone);
+
+            Vector2D[] dlist =
+            {
+                new Vector2D(-1, 0),
+                new Vector2D(0, -1),
+                new Vector2D(1, 0),
+                new Vector2D(0, 1)
+            };
+            
+
+            for(int i = 0; i < dlist.Length; i++)
+            {
+                Random rd = new Random(DateTime.Now.Millisecond);
+                var tmp = dlist[i];
+                var idx = rd.Next(0, dlist.Length - 1);
+                dlist[i] = dlist[idx];
+                dlist[idx] = tmp;
+            }
+
+            foreach (var dir in dlist)
+            {
+                int dx = dir.x;
+                int dy = dir.y;
+
+                if (getValue(x + dx*2, y + dy+2) == WallTypeWall)
+                {
+                    Set(x + dx, y + dy, WallTypeNone);
+                    Dig(x + dx * 2, y + dy + 2);
+                }
+            }
+        }
+    }
+}
